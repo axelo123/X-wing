45f17e6edff8d120b76a1c9156f8fb4ab428b5ba
Axel Verbeiren <axel.verbeiren@gmail.com>
2015-12-05 15:42:42 +0100

    integration à GIT

---
+++ Core/App.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDB;
+
+namespace X_wing.Core
+{
+    class App
+    {
+        #region Members
+
+        protected static MyDB.MyDB BDD;
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Constructor
+
+
+
+        #endregion
+
+        #region Methods
+
+        public static MyDB.MyDB ConnecterBD()
+        {
+            BDD = new MyDB.MyDB("stock", "b5NmmVrLM8VGL4dx", "stock", "localhost");
+            return BDD; 
+        }
+
+        public static List<MyDB.MyDB.IRecord> recuperation(string nomTable)
+        {
+
+            List<MyDB.MyDB.IRecord> enreg = new List<MyDB.MyDB.IRecord>();
+            string query = string.Format("SELECT * FROM {0} ", nomTable);
+
+            foreach (MyDB.MyDB.IRecord elem in BDD.Read(query))
+            {
+                enreg.Add(elem);
+            }
+            return enreg;
+        }
+
+        #endregion
+    }
+}
---
+++ Model/Utilisateur.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using X_wing.Core;
+
+namespace X_wing.Model
+{
+    class Utilisateur : ModelCore
+    {
+        #region Members
+
+        protected string NomTable = "utilisateur";
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Constructor
+
+        private Utilisateur(string id) :base(id)
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public int Role(Role role, int id_utilisateur, int id)
+        {
+            return 1;
+        }
+
+        public int Carte_vaisseau_pilote(Carte_vaisseau_pilote CVP, int id_utilisateur, int id)
+        {
+            return 1;
+        }
+
+        public int Figurine(Figurine figurine, int id_utilisateur, int id)
+        {
+            return 1;
+        }
+
+        public int Amelioration(Amelioration amelioration, int id_utilisateur, int id)
+        {
+            return 1;
+        }
+
+        #endregion
+    }
+}
